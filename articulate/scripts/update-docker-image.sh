c374782f00ae5ab54f1721c7496c17de1a3eea0a
Daniel L. Buchko <dbuchko@pivotal.io>
2018-08-13 01:14:13 -0300

    Fixes to get docker image update working.

--- articulate/scripts/update-docker-image.sh
+++ articulate/scripts/update-docker-image.sh
@@ -2,7 +2,13 @@
 
 # This script will execute in Jenkins from the project root directory, so we need to watch the paths carefully
 
+docker_tgt_dir=articulate/target/docker
+mkdir $docker_tgt_dir
+
+cp articulate/scripts/Dockerfile $docker_tgt_dir
+cp articulate/target/articulate-0.0.3-SNAPSHOT.jar $docker_tgt_dir
+
 # Update the docker image
-docker build articulate/docker -t dbuchko/articulate
+docker build $docker_tgt_dir -t dbuchko/articulate
 docker push dbuchko/articulate:latest
 
